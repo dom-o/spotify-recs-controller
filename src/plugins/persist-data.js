527b2951977a9c91638eaa464e45ae6dc273bc31
Domonic Milesi <dom-o@users.noreply.github.com>
2020-08-14 12:00:37 -0400

    Spotify auth/refresh tokens now expire after a day.

--- src/plugins/persist-data.js
+++ src/plugins/persist-data.js
@@ -1,23 +1,22 @@
+const persistInterval = 1000 * 60 * 60 * 24
+
 const plugin = store => {
   store.subscribe((mutation, state) => {
     if(mutation.type=== 'updateSongRecs') {
       try {
         localStorage.setItem('song_recs', JSON.stringify(state.song_recs))
       } catch(error) {}
-    } else if(mutation.type === 'addSong' ||
-      mutation.type === 'removeSong')
+    } else if(mutation.type === 'addSong' || mutation.type === 'removeSong')
     {
       try {
         localStorage.setItem('seed_songs', JSON.stringify(state.seed_songs))
       } catch(error) {}
-    } else if(mutation.type === 'addArtist' ||
-      mutation.type === 'removeArtist')
+    } else if(mutation.type === 'addArtist' || mutation.type === 'removeArtist')
     {
       try {
         localStorage.setItem('seed_artists', JSON.stringify(state.seed_artists))
       } catch(error) {}
-    } else if(mutation.type === 'addGenre' ||
-      mutation.type === 'removeGenre')
+    } else if(mutation.type === 'addGenre' || mutation.type === 'removeGenre')
     {
       try {
         localStorage.setItem('seed_genres', JSON.stringify(state.seed_genres))
@@ -33,22 +32,24 @@ const plugin = store => {
     } else if(mutation.type === 'setAccessToken') {
       try {
         localStorage.setItem('access_token', JSON.stringify(state.access_token))
+        localStorage.setItem('auth_ts', new Date().getTime() + persistInterval)
       } catch (error) {}
     } else if(mutation.type === 'setRefreshToken') {
       try{
         localStorage.setItem('refresh_token', JSON.stringify(state.refresh_token))
+        localStorage.setItem('auth_ts', new Date().getTime() + persistInterval)
       } catch (error) {}
-    } else if(mutation.type === 'retrieveData') {
+    } else if(mutation.type === 'retrieveState') {
       retrieveStorage('song_recs', 'updateSongRecs', store)
       retrieveStorage('audio_features', 'setAudioFeatures', store)
       retrieveStorage('seed_artists', 'setSeedArtists', store)
       retrieveStorage('seed_genres', 'setSeedGenres', store)
       retrieveStorage('seed_songs', 'setSeedSongs', store)
-      retrieveStorage('access_token', 'setAccessToken', store)
-      retrieveStorage('refresh_token', 'setRefreshToken', store)
+      retrieveStorage('access_token', 'setAccessToken', store, 'auth_ts')
+      retrieveStorage('refresh_token', 'setRefreshToken', store, 'auth_ts')
     } else if(mutation.type === 'retrieveAuthState') {
-      retrieveStorage('access_token', 'setAccessToken', store)
-      retrieveStorage('refresh_token', 'setRefreshToken', store)
+      retrieveStorage('access_token', 'setAccessToken', store, 'auth_ts')
+      retrieveStorage('refresh_token', 'setRefreshToken', store, 'auth_ts')
     } else if (mutation.type === 'resetSearchState') {
       localStorage.removeItem('song_recs')
       localStorage.removeItem('audio_features')
@@ -58,23 +59,40 @@ const plugin = store => {
     } else if(mutation.type === 'resetAuthState') {
       localStorage.removeItem('refresh_token')
       localStorage.removeItem('access_token')
+      localStorage.removeItem('auth_ts')
     } else if(mutation.type === 'clearStorage') {
       localStorage.clear()
     }
   })
 }
 
-function retrieveStorage(item_name, mutation, store) {
-  let storage = null
+function retrieveStorage(item_name, mutation, store, timestamp_name=null) {
+  let storage = null, timestamp = null
   try {
     storage = localStorage.getItem(item_name)
-    if(storage) {
-      storage = JSON.parse(storage)
-    }
+
+    if(storage) { storage = JSON.parse(storage) }
   } catch(error) {}
 
-  if(storage) {
-    store.commit(mutation, storage)
+  if(timestamp_name) {
+    try {
+      timestamp = localStorage.getItem(timestamp_name)
+      if (timestamp) { timestamp = JSON.parse(timestamp)
+      }
+    } catch(error) {}
+  }
+
+  if (storage) {
+    if (timestamp_name) {
+      if (timestamp && new Date().getTime() < timestamp) {
+        store.commit(mutation, storage)
+      } else {
+        localStorage.removeItem(item_name)
+        localStorage.removeItem(timestamp_name)
+      }
+    } else {
+      store.commit(mutation, storage)
+    }
   }
 }
 
