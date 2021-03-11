0651c1e5f1dc75daca4cba6187766c46466e6c43
Domonic Milesi <dom-o@users.noreply.github.com>
2021-03-11 00:38:08 -0500

    Add link back to main site.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
+  <div>
+  <header>Back to <a href="https://domonicmilesi.com/">domonicmilesi.com</a></header>
   <div id="app">
+
     <router-view></router-view>
   </div>
+</div>
 </template>
 
 <script>
